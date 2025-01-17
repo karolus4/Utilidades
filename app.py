3f73aca6bcec1f61482368ab7c32e25c66bbfced
Karolus4 <karolus_aqp@yahoo.com>
2025-01-17 15:37:50 -0500

    Eliminacion de Quitar Fondo por problemas con rembg

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from shutil import rmtree
 import streamlit as st 
 from streamlit_option_menu import option_menu
 from PIL import Image
-from rembg import remove
 import yt_dlp
 
 # Funciones ####################################################
